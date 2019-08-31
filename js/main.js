b90153caebf151652a6186299e647a8fcb3482bb
547h1J <jobayerhossain977@Gmail.com>
2019-08-31 23:03:44 +0600

    Final Finished

--- js/main.js
+++ js/main.js
@@ -1,86 +1,81 @@
-
-
-
-$(document).ready(function() {
-    
-  
-            // owl-carousel homeSliderSection
+$(document).ready(function () {
+    // owl-carousel homeSliderSection
     $("#homeSliderSection").owlCarousel({
-    	items:1,
-    	nav:true,
-    	dots:false,
-    	loop:true,
-    	autoPlay:false
-
+        items: 1,
+        nav: true,
+        dots: true,
+        loop: true,
+        autoPlay: false,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1
+            }
+        }
     });
-   // owl-carousel hotdealSlider
+    // owl-carousel hotdealSlider
     $("#hotdealSlider").owlCarousel({
-        items:5,
-        nav:true,
-        autoPlay:false,
-        margin:15,
-        dots:true,
+        items: 5,
+        nav: true,
+        autoPlay: false,
+        margin: 15,
+        dots: true,
         navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive:{
-            0:{
-                items:1,
-                nav:false
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
             },
-            600:{
-                items:2,
-                nav:false
+            600: {
+                items: 2,
+                nav: false
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
-
     });
-  // owl-carousel testmonial-Section
+    // owl-carousel testmonial-Section
     $("#testmonial-Slider").owlCarousel({
-        items:2,
-        nav:true,
-        autoPlay:false,
-        margin:15,
-        dots:true,
+        items: 2,
+        nav: true,
+        autoPlay: false,
+        margin: 15,
+        dots: true,
         navText: [
-        '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
-        '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
-    ],
-    responsive:{
-            0:{
-                items:1,
-                nav:false
+            '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+            '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
             },
-            600:{
-                items:1,
-                nav:false
+            600: {
+                items: 1,
+                nav: false
             },
-            1000:{
-                items:2
+            1000: {
+                items: 2
             }
         }
-
     });
-  
-
-
-
-
 });
-
-
- function openNav() {
-  // document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main-menu").style.display = "block";
-  
+function openNav() {
+    // document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main-menu").style.display = "block";
 }
-
 function closeNav() {
-  // document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main-menu").style.display = "none";
-  
+    // document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main-menu").style.display = "none";
 }
