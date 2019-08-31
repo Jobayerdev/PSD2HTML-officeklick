fd6f238ed5eb6036ca5922153a8a8f755853676d
547h1J <jobayerhossain977@Gmail.com>
2019-08-31 15:57:51 +0600

    Project Upload 01

---
+++ JPGTOHTML/js/main.js
@@ -0,0 +1,82 @@
+
+
+
+$(document).ready(function() {
+    
+  
+            // owl-carousel homeSliderSection
+    $("#homeSliderSection").owlCarousel({
+    	items:1,
+    	nav:true,
+    	dots:false,
+    	loop:true,
+    	autoPlay:false
+
+    });
+   // owl-carousel hotdealSlider
+    $("#hotdealSlider").owlCarousel({
+        items:5,
+        nav:true,
+        autoPlay:false,
+        margin:15,
+        dots:true,
+        navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+
+    });
+  // owl-carousel testmonial-Section
+    $("#testmonial-Slider").owlCarousel({
+        items:2,
+        nav:true,
+        autoPlay:false,
+        margin:15,
+        dots:true,
+        navText: [
+        '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+    ],
+    responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
+        }
+
+    });
+  
+
+
+
+
+});
+
+
+ function openNav() {
+  // document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main-menu").style.display = "block";
+  
+}
+
+function closeNav() {
+  // document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main-menu").style.display = "none";
+  
+}
